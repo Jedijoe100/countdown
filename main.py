904554df539027ee57c5bc31911db8e34404b06b
joe <thisisjedijoe@gmail.com>
2025-03-02 13:15:01 +1300

    Added an explaination to explain the game to the player

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 """
 TODO Input Sanatisation
 TODO Layout
-TODO Game Testing
+TODO Explaination
+
+Possible adjustments:
+- Better dictionary
+- Generate a set collection of words and pull without replacement
+- Adjust common letters to that of 9 letter words not the entire collection
+- Highscore/saving history
+- Record Maximum possible score
 """
 
 import tkinter as tk
 import tkinter.scrolledtext as tkst
 import numpy as np
+import os
 from processes.dictionary import Dictionary, compact_letters, compute_letters
 
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 MINIMUM_CONSONANTS = 4
 MINIMUM_VOWELS = 3
 LETTER_TOTAL = 9
 START_TIME = 30
 NUMBER_OF_ROUNDS = 4
+EXPLAINATION_FILE = 'explaination.txt'
 
 class Main(tk.Tk):
     def __init__(self):
+        """
+        Initialise the program
+        """
         super().__init__()
         self.dictionary = Dictionary(LETTER_TOTAL)
         self.letter_array = np.zeros(26)
@@ -28,13 +41,19 @@ class Main(tk.Tk):
         self.time = START_TIME
         self.score = 0
         self.round = 1
-        self.build_ui()
         self.history = "Round 1"
+        self.explaintation = ""
+        with open(os.path.join(FILE_PATH, EXPLAINATION_FILE)) as file:
+            self.explaination = file.read().format(LETTER_TOTAL = LETTER_TOTAL, MINIMUM_CONSONANTS=MINIMUM_CONSONANTS, MINIMUM_VOWELS=MINIMUM_VOWELS, NUMBER_OF_ROUNDS= NUMBER_OF_ROUNDS, START_TIME=START_TIME, TWICE_LETTER_TOTAL= LETTER_TOTAL*2)
+        self.build_ui()
     
     def build_ui(self):
+        """
+        Generate the User Interface
+        """
         self.title("Countdown")
-        self.geometry('400x600')
-        self.explaination = tk.Label(self, text='Explaination text')
+        self.geometry('600x600')
+        self.explaination = tk.Label(self, text=self.explaination)
         self.explaination.pack()
         self.score_label = tk.Label(self, text='Score: 0')
         self.score_label.pack()
@@ -64,12 +83,18 @@ class Main(tk.Tk):
         self.history_label.configure(state = "disabled")
 
     def add_to_history(self, text):
+        """
+        Subfunction to add to the program history
+        """
         self.history_label.configure(state = "normal")
         self.history_label.insert(tk.INSERT, text+"\n")
         self.history_label.configure(state = "disabled")
         self.history_label.see('end')
     
     def new_game(self):
+        """
+        Resets the program for the user to play again
+        """
         self.letter_array = np.zeros(26)
         self.letters = []
         self.vowels = 0
@@ -90,6 +115,9 @@ class Main(tk.Tk):
         self.add_to_history(f"Round {self.round}")
 
     def next_round(self):
+        """
+        Progresses the game to the next round
+        """
         self.round += 1
         self.letter_array = np.zeros(26)
         self.letters = []
@@ -108,12 +136,16 @@ class Main(tk.Tk):
 
 
     def end_game(self):
+        """
+        Computes the longest possible words and checks the players word at the end of a round
+        """
         game_letters = compute_letters(self.letters)
         longest_words = self.dictionary.find_longest_word(game_letters)
         player_word = self.word_box.get('0.0', 'end').strip()
-        #check if player word only contains letters, remove uppercase
+        #need to check if player word only contains letters, remove uppercase
         player_letters = compute_letters(player_word)
         string = f"Player Word: {player_word},"
+        #processing and displaying in history the round
         if np.any(game_letters - player_letters < 0):
             string += ' invalid letters, Word Score: 0'
         elif player_word not in self.dictionary.word_set:
@@ -121,6 +153,7 @@ class Main(tk.Tk):
         else:
             score = len(player_word)
             if score == LETTER_TOTAL:
+                #so that the player scores double (18 if 9 letters) when they get the maximum length word
                 score *= 2
             string += f' valid word, Word Score: {score}'
             self.score += score
@@ -138,10 +171,16 @@ class Main(tk.Tk):
         
 
     def start_loop(self):
+        """
+        Starts the gameloop
+        """
         self.game_loop()
         self.add_to_history(f"Letters {''.join(self.letters)}")
 
     def game_loop(self):
+        """
+        Reduces the timer calling itself until the time is finished
+        """
         self.time -= 1
         self.timer_label['text'] = f"{self.time}s"
         if self.time > 0:
@@ -150,14 +189,21 @@ class Main(tk.Tk):
             self.end_game()
     
     def display_letters(self):
+        """
+        Displays the letters
+        """
         string = "".join(self.letters)
         self.selected_elements['text'] = string
 
     def add_vowel(self):
+        """
+        Adds a vowel to the letter list
+        """
         self.letters.append(self.dictionary.random_vowel())
         self.vowels += 1
         self.letter_number += 1
         self.display_letters()
+        #the following enables and disables buttons depending on whether there are enough letters or if the rest of the letters need to be consonants
         if self.consonants < MINIMUM_CONSONANTS and LETTER_TOTAL - self.letter_number == MINIMUM_CONSONANTS - self.consonants:
             self.vowel_button['state'] = tk.DISABLED
         if LETTER_TOTAL == self.letter_number:
@@ -167,10 +213,14 @@ class Main(tk.Tk):
 
     
     def add_consonant(self):
+        """
+        Adds a consonant to the letter list
+        """
         self.letters.append(self.dictionary.random_consonant())
         self.consonants += 1
         self.letter_number += 1
         self.display_letters()
+        #the following enables and disables buttons depending on whether there are enough letters or if the rest of the letters need to be vowel
         if self.vowels < MINIMUM_VOWELS and LETTER_TOTAL - self.letter_number == MINIMUM_VOWELS - self.vowels:
             self.consonant_button['state'] = tk.DISABLED
         if LETTER_TOTAL == self.letter_number:
